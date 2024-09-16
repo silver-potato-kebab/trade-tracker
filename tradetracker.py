dfdd4180d517f4e40cd2ebf35117370ef425c028
silver-potato-kebab- <181072123+silver-potato-kebab@users.noreply.github.com>
2024-09-16 02:25:05 -0700

    Add csv support to tradetracker

--- tradetracker.py
+++ tradetracker.py
@@ -1,3 +1,5 @@
+import csv
+import os.path
 import tkinter as tk
 
 from tkinter import ttk
@@ -7,7 +9,9 @@ from edit_treeview import EditTreeview
 
 
 class TradeTracker(tk.Tk):
+    """Application for tracking trades. Can import and export transactions in a CSV file."""
     def __init__(self, name: str):
+        """Constructor for TradeTracker. Instsantiate various widgets for the user interface."""
         super().__init__()
 
         self.title(name)
@@ -17,9 +21,12 @@ class TradeTracker(tk.Tk):
 
         self.main_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
-    def build_ui(self, master):
+        self.import_csv_file()
 
+    def build_ui(self, master):
+        """Create the graphical user interface on a master widget."""
         column_names = {
+            "open_date": "Date",
             "ticker": "Ticker",
             "long_short": "Long/Short",
             "open_shares": "Shares",
@@ -34,15 +41,40 @@ class TradeTracker(tk.Tk):
             "profit_loss": "Profit/Loss",
             "net_percentage": "Net %"}
 
-        treeview = EditTreeview(master=master, columns=list(column_names.keys()))
-        treeview.heading(column="#0", text="Date")
-        treeview.column(column="#0", width=100)
+        self.treeview = EditTreeview(master=master, columns=list(column_names.keys()), show="headings")
 
         for col_name, col_text in column_names.items():
-            treeview.heading(column=col_name, text=col_text)
-            treeview.column(column=col_name, width=100)
+            self.treeview.heading(column=col_name, text=col_text)
+            self.treeview.column(column=col_name, width=100)
 
-        treeview.pack(fill=tk.BOTH, expand=True)
+        self.treeview.pack(fill=tk.BOTH, expand=True)
+
+    def export_csv_file(self):
+        """Export to a CSV file."""
+        file_exists = os.path.isfile("./test_tradetracker.csv")
+        header = ("open_date","ticker", "long_short", "open_shares", "open_price", "net_cost", "close_date", "close_shares", "close_price", "net_proceeds")
+
+        with open("test_tradetracker.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames=header)
+
+            if not file_exists:
+                writer.writeheader()
+
+            pass
+
+    def import_csv_file(self):
+        """Import from a CSV file."""
+        file_path = tk.filedialog.askopenfilename(title="Open CSV File", filetypes=[("CSV files", "*.csv")])
+
+        if file_path:
+            try:
+                with open(file_path, "r", newline="") as file:
+                    csv_reader = csv.DictReader(file)
+                    for row in csv_reader:
+                        self.treeview.insert(parent="", index=tk.END, values=("1", "2"))
+
+            except Exception as e:
+                print(f"Error: {e}")
 
 
 if __name__ == "__main__":
